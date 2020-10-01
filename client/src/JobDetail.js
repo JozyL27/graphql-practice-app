b603b5ff59d0700d03bc2e63cafbf99baa819e4e
Jose <jozylopez27@gmail.com>
2020-10-01 14:11:03 -0500

    added load job request, func to job comp

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import { loadJob } from './requests'
 
 export class JobDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {job: null, loading: false};
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
     const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
+    const job = await loadJob(jobId)
+    this.setState({ job: job, loading: false })
   }
 
   render() {
-    const {job} = this.state;
+    const {job, loading} = this.state;
+    if(!job && loading === false) {
+      return <p>No Job was found</p>
+    } else if(loading === true && !job) {
+      return <p>loading....</p>
+    } else {
     return (
       <div>
         <h1 className="title">{job.title}</h1>
@@ -20,5 +31,6 @@ export class JobDetail extends Component {
         <div className="box">{job.description}</div>
       </div>
     );
+    }
   }
 }
--- client/src/requests.js
+++ client/src/requests.js
@@ -1,5 +1,31 @@
 const endpoint = 'http://localhost:9000/graphql'
 
+export const loadJob = async (id) => {
+    const res = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: `query JobQuery($id: ID!){
+                job(id: $id) {
+                  id
+                  title
+                  company {
+                    id
+                    name
+                  }
+                  description
+                }
+              }`,
+            variables: { id }
+        })
+    })
+
+    const body = await res.json()
+    return body.data.job
+}
+
 export const loadJobs = async () => {
     const res = await fetch(endpoint, {
         method: 'POST',
