bc08b427dce24a704f40c0d9c6b42f53d1f9e55b
Jose <jozylopez27@gmail.com>
2020-10-06 14:44:49 -0500

    added mutation req

--- client/src/CompanyDetail.js
+++ client/src/CompanyDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { loadCompany } from './requests'
+import { JobList } from './JobList'
 
 export const CompanyDetail = (props) =>  {
   const {companyId} = props.match.params
@@ -20,6 +21,8 @@ export const CompanyDetail = (props) =>  {
       <div>
         <h1 className="title">{company.name}</h1>
         <div className="box">{company.description}</div>
+        <h5 className="title is-5">Jobs at {company.name}</h5>
+        <JobList jobs={company.jobs} />
       </div>
     );
 }
--- client/src/requests.js
+++ client/src/requests.js
@@ -1,37 +1,59 @@
-const endpoint = 'http://localhost:9000/graphql'
+const endpoint = "http://localhost:9000/graphql";
 
-const graphqlRequest = async (query, variables={}) => {
-    const res = await fetch(endpoint, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({ query, variables })
-    })
+const graphqlRequest = async (query, variables = {}) => {
+  const res = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ query, variables }),
+  });
 
-    const responseBody = await res.json()
-    if (responseBody.errors) {
-        const message = responseBody.errors.map((error) => error.message).join('\n')
-        throw new Error(message)
-    }
+  const responseBody = await res.json();
+  if (responseBody.errors) {
+    const message = responseBody.errors
+      .map((error) => error.message)
+      .join("\n");
+    throw new Error(message);
+  }
 
-    return responseBody.data
-}
+  return responseBody.data;
+};
+
+export const createJob = async (input) => {
+  const mutation = `mutation CreateJob($input: CreateJobInput) {
+        job: createJob(input: $input) {
+          id
+          title
+          company {
+            id
+            name
+          }
+        }
+    }`;
+
+  const { job } = await graphqlRequest(mutation, { input });
+  return job;
+};
 
 export const loadCompany = async (id) => {
-    const query = `query CompanyQuery($id: ID!){
+  const query = `query CompanyQuery($id: ID!){
         company(id: $id) {
           id
           name
           description
+          jobs {
+              id
+              title
+          }
         }
-    }`
-    const { company } = await graphqlRequest(query, { id })
-    return company
-}
+    }`;
+  const { company } = await graphqlRequest(query, { id });
+  return company;
+};
 
 export const loadJob = async (id) => {
-    const query = `query JobQuery($id: ID!){
+  const query = `query JobQuery($id: ID!){
         job(id: $id) {
           id
           title
@@ -41,13 +63,13 @@ export const loadJob = async (id) => {
           }
           description
         }
-    }`
-    const { job } = await graphqlRequest(query, {id})
-    return job
-}
+    }`;
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+};
 
 export const loadJobs = async () => {
-    const query = `{
+  const query = `{
         jobs {
           id
           title
@@ -56,8 +78,8 @@ export const loadJobs = async () => {
             name
           }
         }
-      }`
+      }`;
 
-    const { jobs } = await graphqlRequest(query)
-    return jobs
-}
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
+};
